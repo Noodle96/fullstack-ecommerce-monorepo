01a308a9c6ef9648a4a2e524a64e2633622e8c9c
Noodle96 <jorgealfredomarino2@gmail.com>
2026-01-21 18:08:59 -0500

    Add responsive footer component

--- client/src/app/components/Footer.tsx
+++ client/src/app/components/Footer.tsx
@@ -1,6 +1,48 @@
+import Image from "next/image"
+import Link from "next/link"
+
 const Footer = () => {
     return (
-        <div className=''>Footer</div>
+        <div className='bg-gray-800 mt-16 flex flex-col items-center gap-8 md:flex-row md:items-start md:justify-between md:gap-0 p-8 rounded-lg'>
+            <div className="flex flex-col gap-4 items-center md:items-start">
+                <Link href="/" className="flex items-center">
+                    <Image
+                        src="/logo.png"
+                        alt="Logo"
+                        width={36}
+                        height={36}
+                    />
+                    <p className="text-md font-medium tracking-wider hidden md:block text-white">E-commerce Clothes</p>
+                </Link>
+                <p className="text-sm text-gray-400">© 2026 E-commerce Clothes.</p>
+                <p className="text-sm text-gray-400">All rights reserved.</p>
+            </div>
+
+            <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+                <p className="text-sm text-amber-50">Links</p>
+                <Link href="/" >HomePage</Link>
+                <Link href="/" >Contact</Link>
+                <Link href="/" >Terms of Service</Link>
+                <Link href="/" >Privacy Policy</Link>
+            </div>
+
+            <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+                <p className="text-sm text-amber-50">Products</p>
+                <Link href="/" >All Products</Link>
+                <Link href="/" >Arrivals</Link>
+                <Link href="/" >Sellers</Link>
+                <Link href="/" >Sale</Link>
+            </div>
+
+
+            <div className="flex flex-col gap-4 text-sm text-gray-400 items-center md:items-start">
+                <p className="text-sm text-amber-50">Company</p>
+                <Link href="/" >About</Link>
+                <Link href="/" >Contact</Link>
+                <Link href="/" >Blog</Link>
+                <Link href="/" >Affiliate Program</Link>
+            </div>
+        </div>
     )
 }
 
